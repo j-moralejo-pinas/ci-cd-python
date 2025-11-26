d625e4bef8ee4169eb47e4eebef962b9ffe12672
Javier Moralejo Piñas <j.moralejo.pinas@gmail.com>
2025-11-26 15:08:46 +0100

    - Fixed: branch creation

--- actions/create-branch/create-branch.sh
+++ actions/create-branch/create-branch.sh
@@ -4,9 +4,10 @@ set -euo pipefail
 
 BRANCH_NAME="$1"
 PAT_TOKEN="$2"
+REPOSITORY="$3"
 
 # Create and push the new branch
 git checkout -b "$BRANCH_NAME"
-git push https://x-access-token:"${PAT_TOKEN}"@github.com/${{ github.repository }}.git "$BRANCH_NAME"
+git push "https://x-access-token:${PAT_TOKEN}@github.com/${REPOSITORY}.git" "$BRANCH_NAME"
 
 echo "Branch '$BRANCH_NAME' created and pushed successfully"
